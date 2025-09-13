6aaf16bbddf1613a504bdeb930ebb58e3dc4c221
Andi Meier <hallo@andimeier.ch>
2025-09-13 16:05:40 +0200

    feat(animations): update in-view animations

--- components/features/projects/Projects.tsx
+++ components/features/projects/Projects.tsx
@@ -25,7 +25,7 @@ export default function Projects({
     projects,
 }: {
     title: string;
-    lead: string;
+    lead?: string;
     projects: Project[];
 }) {
     return (
--- lib/motion.ts
+++ lib/motion.ts
@@ -1,15 +1,14 @@
 import { Variants, stagger } from 'motion/react';
 
+const hidden = { scale: 0.8, opacity: 0, y: 30 };
+const visible = { scale: 1, opacity: 1, y: 0 };
+
 export const viewportContainerVariants: Variants = {
-    hidden: { opacity: 0, y: 30 },
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: { delayChildren: stagger(0.3) },
-    },
+    hidden: hidden,
+    visible: { ...visible, transition: { delayChildren: stagger(0.2) } },
 };
 
 export const viewportChildVariants: Variants = {
-    hidden: { opacity: 0, y: 30 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+    hidden: hidden,
+    visible: { ...visible, transition: { duration: 0.5 } },
 };
